f201ff0f198a473e56306a68159fa134a43c5bf0
chaturvedi-anil <chaturvedia560@gmail.com>
2024-12-07 22:24:47 +0530

    added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Button from './components/ui/Button';
+import PlusIcon from './icons/PlusIcon';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='m-20 bg-black-400'>
+      <Button variant='primary' text='Add Content' size='md' startIcon={<PlusIcon />} />
+      <Button variant='secondary' text='Share' size='md' />
+      <Button variant='secondary' text='Delete' size='xl' />
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import { ReactElement } from "react";
 
-const Button = () => {
+export interface ButtonProps {
+    variant: "primary" | "secondary";
+    size: "sm" | "md" | "lg" | "xl";
+    text: string;
+    startIcon?: ReactElement,
+    endIcon?: ReactElement,
+    onClick?: () => void 
+}
+
+const variantStyles = {
+    "primary" : "bg-purple-600 text-white ",
+    "secondary" : "bg-purple-300 text-purple-600 "
+}
+
+const defaultStyle = "rounded-md m-2 flex";
+
+const sizeStyle ={
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-4 px-6",
+    "xl": "py-4 px-8"
+}
+
+const Button = (props: ButtonProps) => {
+
   return (
-    <div>Button</div>
+    <button className={`${defaultStyle} ${variantStyles[props.variant]} ${sizeStyle[props.size]}`} >
+        {props.startIcon} {props.text} {props.endIcon}
+    </button>
   )
 }
 
