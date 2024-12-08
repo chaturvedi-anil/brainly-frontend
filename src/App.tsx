e1101631eceeafc25d20be3519d310e769bd3d10
chaturvedi-anil <chaturvedia560@gmail.com>
2024-12-08 23:12:27 +0530

    Added sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -15,56 +15,54 @@ const App = () => {
     setIsModalOpen(prevState => !prevState);
   }
   const handleShareLink = () => {};
-  return <>
+  return <div className='w-screeen h-screeen flex'>
+    <Sidebar />
     {isModalOpen 
       ?
         <CreateContentModal open={isModalOpen} onClose={() => setIsModalOpen(false)} /> 
       :
-        <div className='w-screen h-screen'>
-          <div className='m-8 bg-black-400 flex flex-col'>
-            <div className='flex items-center gap-2 justify-end mb-5'>
-              <Button 
-                variant='secondary' 
-                text='Share Brain' 
-                size='md' 
-                startIcon={<ShareIcon size='lg' /> } 
-                onClickHandler={handleShareLink}
-              /> 
-              <Button 
-                variant='primary' 
-                text='Add Content' 
-                size='md' 
-                startIcon={<PlusIcon size='lg' />} 
-                onClickHandler={handleOpenModal} 
-              />
-            </div>
-        
+        <div className='w-screen m-8 bg-black-400 flex flex-col'>
+          <div className='flex items-center gap-2 justify-end mb-5'>
+            <Button 
+              variant='secondary' 
+              text='Share Brain' 
+              size='md' 
+              startIcon={<ShareIcon size='lg' /> } 
+              onClickHandler={handleShareLink}
+            /> 
+            <Button 
+              variant='primary' 
+              text='Add Content' 
+              size='md' 
+              startIcon={<PlusIcon size='lg' />} 
+              onClickHandler={handleOpenModal} 
+            />
+          </div>
 
-            <div className='flex gap-4'>
-              <Cart 
-                title='First youbtube video' 
-                linkIcon={<ShareIcon size='lg' />} 
-                type='youtube'
-                link={'https://www.youtube.com/embed/mGpBaVPsX8U?si=lGIwvGtHoG0L5Jps'}
-                tags={["productivity", "c++", "java"]}
-                shareIcon={<ShareIcon size='lg' />}
-                trashIcon={<TrashIcon size='lg' />}
-              />
+          <div className='flex gap-4'>
+            <Cart 
+              title='First youbtube video' 
+              linkIcon={<ShareIcon size='lg' />} 
+              type='youtube'
+              link={'https://www.youtube.com/embed/mGpBaVPsX8U?si=lGIwvGtHoG0L5Jps'}
+              tags={["productivity", "c++", "java"]}
+              shareIcon={<ShareIcon size='lg' />}
+              trashIcon={<TrashIcon size='lg' />}
+            />
 
-              <Cart 
-                title='First twitter post' 
-                linkIcon={<ShareIcon size='lg' />} 
-                type='twitter'
-                link={'https://twitter.com/Chaturvedi_99/status/1836803107306836267?ref_src=twsrc%5Etfw'}
-                tags={["productivity", "c++", "java"]}
-                shareIcon={<ShareIcon size='lg' />}
-                trashIcon={<TrashIcon size='lg' />}
-              />
-            </div>
+            <Cart 
+              title='First twitter post' 
+              linkIcon={<ShareIcon size='lg' />} 
+              type='twitter'
+              link={'https://twitter.com/Chaturvedi_99/status/1836803107306836267?ref_src=twsrc%5Etfw'}
+              tags={["productivity", "c++", "java"]}
+              shareIcon={<ShareIcon size='lg' />}
+              trashIcon={<TrashIcon size='lg' />}
+            />
           </div>
         </div> 
     }
-  </>
+  </div>
 }
 
 export default App;
--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,13 +1,14 @@
+import SunIcon from "../../icons/SunIcon"
 
 const Sidebar = () => {
   return (
-    <div className="w-96 h-screen bg-red-300 px-6">
-       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea nulla, rerum adipisci repudiandae quibusdam. Ipsum similique corporis necessitatibus perspiciatis cum vel doloribus sit blanditiis aspernatur, delectus consequuntur rem quas?
-            Repudiandae, iure adipisci. Laudantium quo dolorum in ipsa voluptas esse facilis, ab repellat perferendis sint exercitationem error voluptatem dolor iure minus ex, quibusdam nam atque? Soluta similique eligendi debitis illum!
-            Soluta impedit, quaerat deleniti quos veniam quasi rem debitis exercitationem consequatur, vel animi libero fugiat et velit recusandae? Repellendus dolorum incidunt voluptate eum atque voluptatem eaque sequi. Quos, assumenda dolor.
-            Corporis odio autem nulla itaque illum sunt nobis sapiente, accusamus aliquid velit accusantium saepe, consequatur blanditiis culpa similique dolores alias facere possimus, atque veritatis temporibus suscipit ipsum vitae numquam. Necessitatibus?
-            Error quae numquam molestias ducimus ipsam alias obcaecati dolor accusamus aliquid. Maxime nam veniam neque fuga accusantium, voluptatem dolorum ea rem deserunt, at quibusdam. Qui repudiandae corporis culpa esse libero.
+    <div className="h-screen bg-white w-3/12 border-r shadow-inner position-fixed left-0 top-0">
+       <div className="flex items-center border-b-2 py-4 px-4">
+            <SunIcon size="xl"/>
+            <span className="text-purple-600 font-bold text-3xl ml-4">Brainly</span>
+       </div>   
+       <div className="flex">
+
        </div>
     </div>
   )
